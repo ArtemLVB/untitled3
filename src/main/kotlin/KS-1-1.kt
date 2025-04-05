4ce55fabce012584f7ad53fb4202a5cf0c81ae0c
HAIER <dhaus403@gmail.com>
2025-04-05 07:44:07 +0300

    KS-1-1-new create smth

--- src/main/kotlin/KS-1-1.kt
+++ src/main/kotlin/KS-1-1.kt
@@ -1,10 +1,10 @@
 package org.example
 
 fun main() {
-    //
     val totalOrderCount: Int = 75
     val thankYouMessage: String = "Спасибо за вашу покупку!Ждём вас снова"
     println("Количество заказов: $totalOrderCount")
     println(thankYouMessage)
+    //
 
 }
